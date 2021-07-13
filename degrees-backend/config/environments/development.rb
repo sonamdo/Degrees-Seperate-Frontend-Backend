a2e71b9838b04f808dfda80b889981036c826797
sonamdo <sonamklein@gmail.com>
2021-07-13 18:52:24 -0400

    fixed connection issue

--- degrees-backend/config/environments/development.rb
+++ degrees-backend/config/environments/development.rb
@@ -1,6 +1,10 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  #whitelist all ips
+  config.web_console.whitelisted_ips =   '0.0.0.0/0.0.0.0'
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
